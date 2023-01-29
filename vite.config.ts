944e4dd5480291815d56d7e785c239a2873c8fe9
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2023-01-29 11:01:31 -0300

    add vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import VueMacros from "unplugin-vue-macros/vite";
 import vue from "@vitejs/plugin-vue";
@@ -11,5 +12,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    css: false,
   },
 });
