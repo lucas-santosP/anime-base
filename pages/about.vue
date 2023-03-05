3251d26287bdf3feaddbe02ab7c03b7af013c2c6
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2023-03-04 21:46:04 -0300

    fix: about fetch

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { useQuery } from "@tanstack/vue-query";
 
-const fetcher = async () =>
-  await fetch("https://jsonplaceholder.typicode.com/posts").then((response) => response.json());
+const fetcher = async () => {
+  await new Promise((resolve) => setTimeout(() => resolve(true), 800));
 
-const { data, suspense } = useQuery({ queryKey: ["test"], queryFn: fetcher });
+  return await fetch("https://jsonplaceholder.typicode.com/posts").then((response) =>
+    response.json()
+  );
+};
 
-await suspense();
+const { data } = useQuery({ queryKey: ["test"], queryFn: fetcher });
 </script>
 
 <template>
